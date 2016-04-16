5a762f1a3afd0a035bc1eb92a203c91d4beda7c0
Tom Adams <tom@holizz.com>
2016-04-16 10:56:58 -0400

    Add REPL as main()

---
+++ repl.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+
+	"gopkg.in/readline.v1"
+)
+
+func main() {
+	rl, err := readline.New("Toy Lisp REPL> ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rl.Close()
+
+	for {
+		line, err := rl.Readline()
+		if err == io.EOF || err == readline.ErrInterrupt {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+
+		tokens, err := tokenizer(line)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		ast, err := parse(tokens)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		output, err := exec(ast, DefaultFunctions)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%#v\n", output)
+	}
+}
